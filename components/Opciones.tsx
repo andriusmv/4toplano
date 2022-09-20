f68110d2c1d139ac777851cb4f357c3583b87f38
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2022-09-20 15:06:21 -0500

    fixed avatars

--- components/Opciones.tsx
+++ components/Opciones.tsx
@@ -1,5 +1,5 @@
-import { IconTableShortcut, IconTableOptions, IconLogout, IconPoint } from '@tabler/icons';
-import { Avatar, Badge, Box, NavLink } from '@mantine/core';
+import { IconTableShortcut, IconTableOptions, IconLogout, IconPoint, IconBrandTwitter } from '@tabler/icons';
+import { Avatar, Badge, Box, Button, NavLink } from '@mantine/core';
 import Link from 'next/link';
 
 export default function Opciones() {
@@ -9,10 +9,99 @@ export default function Opciones() {
     icon={<IconTableOptions size={16} stroke={1.5} />}
     childrenOffset={28}
   >
-    <NavLink label="Pequeños (hasta 1 Ha)" icon={<Avatar src="E85C90.jpg" alt="pequeños" size="xs" />}/>
-    <NavLink label="Medianos (hasta 10 Ha)" icon={<Avatar src="B88DAF.jpg" alt="medianos" size="xs" />}/>
-    <NavLink label="Grandes (hasta 100 Ha)" icon={<Avatar src="88BECD.jpg" alt="grandes" size="xs" />}/>
-    <NavLink label="Muy Grandes (hasta 500 Ha)" icon={<Avatar src="58EFEC.jpg" alt="muy grandes" size="xs" />}/>
+    <NavLink label="Pequeños (hasta 1 Ha)" icon={<Button
+      component="a"
+      target="_blank"
+      rel="noopener noreferrer"
+      styles={(theme) => ({
+        root: {
+          backgroundColor: '#e85c90',
+          border: 0,
+          height: 20,
+          width: 20,
+          paddingLeft: 5,
+          paddingRight: 5,
+
+          '&:hover': {
+            backgroundColor: theme.fn.darken('#e85c90', 0.05),
+          },
+        },
+
+        leftIcon: {
+          marginRight: 15,
+        },
+      })}
+    />} />
+    <NavLink label="Medianos (hasta 10 Ha)" icon={<Button
+      component="a"
+      target="_blank"
+      rel="noopener noreferrer"
+      styles={(theme) => ({
+        root: {
+          backgroundColor: '#b88daf',
+          border: 0,
+          height: 20,
+          width: 20,
+          paddingLeft: 5,
+          paddingRight: 5,
+
+          '&:hover': {
+            backgroundColor: theme.fn.darken('#b88daf', 0.05),
+          },
+        },
+
+        leftIcon: {
+          marginRight: 15,
+        },
+      })}
+    />}/>
+    <NavLink label="Grandes (hasta 100 Ha)" icon={<Button
+      component="a"
+      target="_blank"
+      rel="noopener noreferrer"
+      styles={(theme) => ({
+        root: {
+          backgroundColor: '#88becd',
+          border: 0,
+          height: 20,
+          width: 20,
+          paddingLeft: 5,
+          paddingRight: 5,
+
+          '&:hover': {
+            backgroundColor: theme.fn.darken('#88becd', 0.05),
+          },
+        },
+
+        leftIcon: {
+          marginRight: 15,
+        },
+      })}
+    />}/>
+    <NavLink label="Muy Grandes (hasta 500 Ha)" icon={<Button
+      component="a"
+      target="_blank"
+      rel="noopener noreferrer"
+      styles={(theme) => ({
+        root: {
+          backgroundColor: '#58efec',
+          border: 0,
+          height: 20,
+          width: 20,
+          paddingLeft: 5,
+          paddingRight: 5,
+
+          '&:hover': {
+            backgroundColor: theme.fn.darken('#58efec', 0.05),
+          },
+        },
+
+        leftIcon: {
+          marginRight: 15,
+        },
+      })}
+    />}/>
+    
   </NavLink>
   <NavLink
     label="Filtrar por tipo"
