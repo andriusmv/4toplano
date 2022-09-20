1a8756cf9e14c12ebfd02d0646921fdd6556dc36
Andres Moreno <99091918+andriusmv@users.noreply.github.com>
2022-09-20 11:28:13 -0500

    controls update

--- components/Opciones.tsx
+++ components/Opciones.tsx
@@ -1,5 +1,5 @@
-import { IconTableShortcut, IconTableOptions, IconLogout } from '@tabler/icons';
-import { Box, NavLink } from '@mantine/core';
+import { IconTableShortcut, IconTableOptions, IconLogout, IconPoint } from '@tabler/icons';
+import { Avatar, Badge, Box, NavLink } from '@mantine/core';
 import Link from 'next/link';
 
 export default function Opciones() {
@@ -9,17 +9,17 @@ export default function Opciones() {
     icon={<IconTableOptions size={16} stroke={1.5} />}
     childrenOffset={28}
   >
-    <NavLink label="Pequeños (hasta 1 Ha)" />
-    <NavLink label="Medianos (hasta 10 Ha)" />
-    <NavLink label="Grandes (hasta 100 Ha)" />
-    <NavLink label="Muy Grandes (hasta 500 Ha)" />
+    <NavLink label="Pequeños (hasta 1 Ha)" icon={<Avatar src="../E85C90.jpg" alt="pequeños" size="xs" />}/>
+    <NavLink label="Medianos (hasta 10 Ha)" icon={<Avatar src="../B88DAF.jpg" alt="medianos" size="xs" />}/>
+    <NavLink label="Grandes (hasta 100 Ha)" icon={<Avatar src="../88BECD.jpg" alt="grandes" size="xs" />}/>
+    <NavLink label="Muy Grandes (hasta 500 Ha)" icon={<Avatar src="../58EFEC.jpg" alt="muy grandes" size="xs" />}/>
   </NavLink>
   <NavLink
     label="Filtrar por tipo"
     icon={<IconTableShortcut size={16} stroke={1.5} />}
     childrenOffset={28}
   >
-    <NavLink label="Lotes Urbanos" />
+    <NavLink label="Lotes Urbanos •" />
     <NavLink label="Lotes Rurales" />
   </NavLink>
   <Link href="./" passHref>
